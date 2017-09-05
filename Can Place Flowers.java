ea7227a6e58ba7902ddb74ad0f51ec3bd2bfb269
Jackpon <JackpongWong@163.com>
2017-09-05 21:33:41 +0800

    add 2 essays about LeetCode

---
+++ Can Place Flowers.java
@@ -0,0 +1,58 @@
+﻿/*
+605. Can Place Flowers
+
+Suppose you have a long flowerbed in which some of the plots are planted and some are not. However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
+
+Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), and a number n, return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
+
+Example 1:
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: True
+Example 2:
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: False
+Note:
+The input array won't violate no-adjacent-flowers rule.
+The input array size is in the range of [1, 20000].
+n is a non-negative integer which won't exceed the input array size.
+
+*/
+
+/*
+比较当前位置左右两边的数是否为0且当前位置是0，是count++；
+但最重要的是处理好首尾两端，eg：[0,0,1,0,0];以及当len<=3的情况
+*/
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+     int count=0,len=flowerbed.length;
+	 //处理len<=3 and 首尾两端 的情况
+        	if (len==1 && flowerbed[0]==0) {
+				return 1>=n;
+			}
+        	if (len==1 && flowerbed[0]==1) {
+				return 0>=n;
+			}
+        	
+        	if (flowerbed[0]==0 && flowerbed[1]==0) 
+        	{	
+        		count++;
+        		flowerbed[0]=1;
+        	}
+        	if (flowerbed[len-1]==0 && flowerbed[len-2]==0) 
+        	{	
+        		count++;
+        		flowerbed[len-1]=1;
+        	}
+	//len>=3 的情况
+        	for (int i =1; i < len-1 && len>=3; i++) {
+				if(flowerbed[i-1]==0 && flowerbed[i+1]==0 && flowerbed[i]==0)
+				{	
+					flowerbed[i]=1;	
+					count++;
+				}
+        	}
+        	
+        	return count>=n;
+    }
+}
---
+++ Maximum Product of Three Numbers.java
@@ -0,0 +1,27 @@
+﻿/*
+628. Maximum Product of Three Numbers
+
+Given an integer array, find three numbers whose product is maximum and output the maximum product.
+
+Example 1:
+Input: [1,2,3]
+Output: 6
+Example 2:
+Input: [1,2,3,4]
+Output: 24
+Note:
+The length of the given array will be in range [3,104] and all elements are in the range [-1000, 1000].
+Multiplication of any three numbers in the input won't exceed the range of 32-bit signed integer.
+*/
+
+//思路其实很简单，将数组排好序，比较sum1和sum2就行
+class Solution {
+    public int maximumProduct(int[] nums) {
+        int len=nums.length;
+		Arrays.sort(nums);
+		int sum1=nums[0]*nums[1]*nums[len-1];
+		int sum2=nums[len-1]*nums[len-2]*nums[len-3];
+		int maxnum=Math.max(sum1,sum2);
+		return maxnum;
+    }
+}
