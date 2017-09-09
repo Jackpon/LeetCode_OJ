f88110b1127c12f38853bbe486e0101d05cfbb53
Jackpon <JackpongWong@163.com>
2017-09-09 09:53:00 +0800

    update

---
+++ Array Partition I.java
@@ -0,0 +1,27 @@
+﻿/*
+561. Array Partition I
+
+Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+
+Example 1:
+Input: [1,4,3,2]
+
+Output: 4
+Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+Note:
+n is a positive integer, which is in the range of [1, 10000].
+All the integers in the array will be in the range of [-10000, 10000].
+
+思路：排序数组，取相邻两个数最小值，步数2遍历
+*/
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int sum=0;
+	        Arrays.sort(nums);
+	        for (int i = 1; i < nums.length; i+=2) {
+				sum += Math.min(nums[i],nums[i-1]);
+			}
+	        return sum;
+    }
+}
---
+++ Search Insert Position.java
@@ -0,0 +1,35 @@
+﻿/*
+35. Search Insert Position
+
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+
+*/
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        //查找target记录位置
+	    int status=Arrays.binarySearch(nums, target);
+	        
+	    //如果target不在数组内，则插入到
+	    if(status<0) {
+	        for (int i = 0; i < nums.length; i++){
+				if (target<nums[i]) {
+					status=i;
+					break;
+				}
+			}
+	        	
+	        if(target > nums[nums.length-1])status=nums.length;
+	    }
+	        
+	    return status;
+    }
+}
