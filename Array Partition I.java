bab1f81120fe4f66eb06fc55f300170ea4cadcf3
Jackpon <JackpongWong@163.com>
2017-09-09 09:54:36 +0800

    Update Array Partition I.java

--- Array Partition I.java
+++ Array Partition I.java
@@ -1,7 +1,8 @@
 ﻿/*
 561. Array Partition I
 
-Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+Given an array of 2n integers, your task is to group these integers into n pairs of integer, 
+say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
 
 Example 1:
 Input: [1,4,3,2]
