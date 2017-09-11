27f86887f944d2df4da8da19d36871914a6d4715
Jackpon <JackpongWong@163.com>
2017-09-11 08:45:10 +0800

    update

---
+++ Contains Duplicate.java
@@ -0,0 +1,41 @@
+﻿/*
+217. Contains Duplicate
+
+Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+*/
+
+ public boolean containsDuplicate(int[] nums) {
+       Arrays.sort(nums);
+	        boolean status=false;
+	        int count=0;
+	        
+	        for (int i = 0; i < nums.length; i++) {
+				for (int j = 0; j < nums.length; j++) { 
+					if (nums[i]==nums[j]) 
+						count++;
+					if(count>1) {
+						status=true;
+						break;
+					}
+				}
+				
+				count=0;
+			}
+	        
+	        return status;
+    }
+	
+//上面的暴力O(n^2)比较很显然是超时的，来个O(n)
+
+ public static boolean containsDuplicate(int[] nums) {
+		 	Arrays.sort(nums);
+	        boolean status=false;
+	        for (int i = 1; i < nums.length; i++) {
+				if(nums[i] == nums[i-1]) {
+					status = true;
+					break;
+				}
+			}
+ 
+	        return status;
+	    }
---
+++ Third Maximum Number.java
@@ -0,0 +1,49 @@
+﻿/*
+414. Third Maximum Number
+
+Given a non-empty array of integers, return the third maximum number in this array. 
+If it does not exist, return the maximum number. The time complexity must be in O(n).
+
+Example 1:
+Input: [3, 2, 1]
+Output: 1
+Explanation: The third maximum is 1.
+
+Example 2:
+Input: [1, 2]
+Output: 2
+Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+
+Example 3:
+Input: [2, 2, 3, 1]
+Output: 1
+Explanation: Note that the third maximum here means the third maximum distinct number.
+Both numbers with value 2 are both considered as second maximum.
+*/
+
+public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+	        int count=0,value=nums[0];
+	        
+	        if (nums.length<3) {
+	        	
+				for (int i = 0; i < nums.length; i++) 
+					value=Math.max(value, nums[i]);
+				
+			}else {
+				
+				for (int i = nums.length-1; i > 0; i--) {
+					if (nums[i]>nums[i-1]) {
+						count++;
+						if (count==2) {
+							value=nums[i-1];
+							break;
+						} 
+					}
+				}
+				if(count<2)
+					value=nums[nums.length-1];
+			}
+	        
+	        return value;
+    }
