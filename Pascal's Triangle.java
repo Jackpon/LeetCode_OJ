b2e260914159f17e0da053f4a521bcbe724ebafe
Jackpon <JackpongWong@163.com>
2017-09-20 22:52:09 +0800

    Pascal's Triangle II

--- Pascal's Triangle.java
+++ Pascal's Triangle.java
@@ -29,3 +29,26 @@ public List<List<Integer>> generate(int numRows) {
     }
     return allrows;
 }
+
+
+/*
+119. Pascal's Triangle II
+
+Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+*/
+
+public List<Integer> getRow(int rowIndex) {
+    List<Integer> row = new ArrayList<Integer>();
+    for (int i = 0; i <= rowIndex; i++) {
+        row.add(0,1);
+        for (int j = 1; j < row.size()-1; j++) 
+            row.set(j,row.get(j) + row.get(j+1));
+    }
+    return row;
+}
